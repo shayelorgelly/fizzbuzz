44015e16f53dccbe527c9653bbe53fe980727fbe
beanybomber <125334533+shayelorgelly@users.noreply.github.com>
2023-11-28 12:26:49 +1300

    make it start at 1

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package net.shayelorgelly;
 //arrow casing requires at least java 12
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 1; i < 101; i++) {
             System.out.println(switch (i % 15) {
                 case 0 -> "FizzBuzz";
                 case 3,6,9,12 -> "Fizz";
