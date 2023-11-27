7c0c6b96fb238a965b07137d6e9dd513e8b9671b
beanybomber <125334533+shayelorgelly@users.noreply.github.com>
2023-11-28 12:32:16 +1300

    fix fizzbuzz and make it start at 1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-for i in range(100):
+for i in range(1, 101):
+    elif i % 15 == 0:
+        print("FizzBuzz")
     if i % 3 == 0:
         print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
-    elif i % 15 == 0:
-        print("FizzBuzz")
     else:
         print(i)
