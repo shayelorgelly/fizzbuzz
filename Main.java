7e21c88ac8fff73c8b3292f1e3d6f89eab14528d
beanybomber <125334533+shayelorgelly@users.noreply.github.com>
2023-11-28 12:02:44 +1300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package net.shayelorgelly;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(switch (i % 15) {
+                case 0 -> "FizzBuzz";
+                case 3,6,9,12 -> "Fizz";
+                case 5,10 -> "Buzz";
+                default -> i;
+            });
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+for (i = 0; i < 100; i++) {
+    console.log(function () {
+        switch (i % 15) {
+            case 0:
+                return "FizzBuzz";
+            case 3:
+            case 6:
+            case 9:
+            case 12:
+                return "Fizz";
+            case 5:
+            case 10:
+                return "Buzz";
+            default:
+                return i
+        }
+    }())
+}
